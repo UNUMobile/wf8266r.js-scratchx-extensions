dfb355106039fac806a8daa93eda0c2048b246ac
UNU Product API <ckenson@gmail.com>
2016-04-11 00:34:35 +0800

    Start

--- wf8266r_extension.js
+++ wf8266r_extension.js
@@ -1,4 +1,7 @@
 (function(ext) {
+    var ip = "";
+    var connection;
+    
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,23 +11,34 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp'];
-                  callback(temperature);
-              }
-        });
+    function socketConnection(ip){
+        connection = new WebSocket('ws://'+ ip +':81/api', ['wf8266r']);
+        connection.onopen = function (e) {
+            //連線成功        
+            console.log("ok");
+            connection.send("gpio/adc");
+        };
+        connection.onclose = function (e) {
+            //連線關閉
+        };
+        connection.onmessage = function (e) {
+            //收到來自 WF8266R 的訊息
+            console.log(e.data);
+        };
+        connection.onerror = function (e) {
+            //不明的錯誤
+        };
+    }
+
+    ext.set_ip = function(text){
+        ip = text;
+        socketConnection(text);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'w current temperature in city %s', 'get_temp', 'Boston, MA'],
+            [' ', '開發板位址 %s', 'set_ip', 'mywf9441.local'],
         ]
     };
 
