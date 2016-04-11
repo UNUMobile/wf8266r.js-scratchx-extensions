201dd5b1a7f4749210017e14781408d776e28b16
UNU Product API <ckenson@gmail.com>
2016-04-11 16:15:47 +0800

    pin mode debug

--- wf8266r_extension.js
+++ wf8266r_extension.js
@@ -16,9 +16,10 @@
         if(!isConnected) return {status: 1, msg: '請設定開發板位址'};
     };
     
-    ext.gpio = function(pin,value){
-        console.log(pin + " " + value);
+    ext.gpio = function(pin,value,callback){
         connection.send("gpio,"+pin+"="+value);
+        var currentCallback = {action:'gpio', index:pin, event:callback};
+        callbackEvent.push(currentCallback);
     };
     
     ext.adc = function(callback){
@@ -75,6 +76,7 @@ console.log(jsonObj);
 console.log(currentCallback);                
             switch(jsonObj.Action)
             {
+                case "gpio" : currentCallback.event(parseInt(eval('jsonObj.D'+currentCallback.index))); break;
                 case "gpio/adc" : currentCallback.event(parseInt(jsonObj.ADC)); break;
                 case "gpio/read" : currentCallback.event(parseInt(eval('jsonObj.D'+currentCallback.index))); break;
                 case "pinmode" : currentCallback.event(parseInt(eval('jsonObj.D'+currentCallback.index))); break;
@@ -92,22 +94,22 @@ console.log(currentCallback);
         blocks: [
             [' ', '開發板位址 %s', 'set_ip', 'mywf9441.local'],
             ['h', '當連線建立時', 'when_connected'],
-            [' ', '腳位 %d.gpio 模式設為 %m.mode', 'pinmode',5,'OUTPUT'],
-            [' ', '腳位 %d.gpio 數位輸出 %m.level', 'gpio',5,1],
-            [' ', '腳位 %d.gpio 類比輸出 %n', 'pwm', 5, 1023],
-            [' ', 'DHT%m.dhtType 溫濕度感測器，接在腳位 %d.gpio' ,'dht', 11,12],
-            [' ', 'DS18B20 溫度感測器，接在腳位 %d.gpio' ,'ds', 4],
-            [' ', 'UART 速率 %m.uartBaud' ,'baud', '115200'],
-            [' ', 'HCSR 超音波感測器，Echo 在腳位 %d.gpio Trig 在腳位 %d.gpio' ,'distance', 5,4],
-            [' ', '紅外線接收器，接在腳位 %d.gpio' ,'irrecv', 14],
-            [' ', '紅外線發射器，接在腳位 %d.gpio 發送資料 %s' ,'irsend', 15, '0'],
-            [' ', '停止紅外線接收' ,'irstop'],
-            [' ', 'UART Tx 送出 %m.uartCode %s 結尾換行 %m.boolType' ,'tx', 'text', 'Hi', 'true'],
-            [' ', '%m.flushType 清空', 'flush', 'UART'],
-            [' ', 'HTTP %m.restfulType 資料 %s 到 %s %s' ,'http', 'POST', 'key=xxxxxx&field1=1&field2=2','api.thingspeak.com', 'update'],
-            [' ', 'HTTP %m.restfulType 資料 %s 從 %s %s' ,'http', 'GET', 'api_key=EM18B52PSHXZB4DD', 'api.thingspeak.com', 'apps/thinghttp/send_request'],
+            ['R', '腳位 %d.gpio 模式設為 %m.mode', 'pinmode',5,'OUTPUT'],
+            ['R', '腳位 %d.gpio 數位輸出 %m.level', 'gpio',5,1],
+            ['R', '腳位 %d.gpio 類比輸出 %n', 'pwm', 5, 1023],
+            ['R', 'DHT%m.dhtType 溫濕度感測器，接在腳位 %d.gpio' ,'dht', 11,12],
+            ['R', 'DS18B20 溫度感測器，接在腳位 %d.gpio' ,'ds', 4],
+            ['R', 'UART 速率 %m.uartBaud' ,'baud', '115200'],
+            ['R', 'HCSR 超音波感測器，Echo 在腳位 %d.gpio Trig 在腳位 %d.gpio' ,'distance', 5,4],
+            ['R', '紅外線接收器，接在腳位 %d.gpio' ,'irrecv', 14],
+            ['R', '紅外線發射器，接在腳位 %d.gpio 發送資料 %s' ,'irsend', 15, '0'],
+            ['R', '停止紅外線接收' ,'irstop'],
+            ['R', 'UART Tx 送出 %m.uartCode %s 結尾換行 %m.boolType' ,'tx', 'text', 'Hi', 'true'],
+            ['R', '%m.flushType 清空', 'flush', 'UART'],
+            ['R', 'HTTP %m.restfulType 資料 %s 到 %s %s' ,'http', 'POST', 'key=xxxxxx&field1=1&field2=2','api.thingspeak.com', 'update'],
+            ['R', 'HTTP %m.restfulType 資料 %s 從 %s %s' ,'http', 'GET', 'api_key=EM18B52PSHXZB4DD', 'api.thingspeak.com', 'apps/thinghttp/send_request'],
             ['R', '讀取數位腳位 %d.gpio' ,'read', 5],
-            ['r', '讀取感測器 %m.sensor 參數 %m.sensorParam' ,'sensor', 'DHT', 'C'],
+            ['R', '讀取感測器 %m.sensor 參數 %m.sensorParam' ,'sensor', 'DHT', 'C'],
             ['R', '讀取類比腳位 ADC','adc'],
         ],
         menus: {
