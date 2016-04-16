e9e0957c9e36158f89b85ef904490b96559bf692
UNU Product API <ckenson@gmail.com>
2016-04-16 13:17:12 +0800

    debug

--- wf8266r_extension 20160417.js
+++ wf8266r_extension 20160417.js
@@ -206,7 +206,7 @@
         var rec = new webkitSpeechRecognition();
         rec.start();
         console.log(rec);
-        rec.continuous = true;
+        rec.continuous = false;
         rec.interimResults = true;
         var result = "";
 
@@ -215,7 +215,7 @@
             if (typeof (event.results) == 'undefined') {
                 rec.onend = null;
                 rec.stop();
-                callback();
+                callback('');
             }
             for (var i = event.resultIndex; i < event.results.length; ++i) {
                 if (event.results[i].isFinal) {
@@ -224,8 +224,8 @@
                     console.log(event.results[i][0].transcript);
                 }
             }
-            console.log(result);
-            callback();
+
+            callback(result);
         }
     }
 
@@ -314,7 +314,7 @@
             ['w', 'HTTP %m.restfulType 到 %s', 'http', 'POST', 'http://api.thingspeak.com/update?key=xxxxxx&field1=1&field2=2'],
             ['w', 'HTTP %m.restfulType 從 %s', 'http', 'GET', 'http://api.thingspeak.com/apps/thinghttp/send_request?api_key=EM18B52PSHXZB4DD'],
             ['w', '說 %s', 'speak_text', 'Scratch 遇上 WF8266R'],
-            ['w', '聽', 'speech_text'],
+            ['w', '聽到', 'speech_text'],
 
             [' ', '紅外線發射器，接在腳位 %d.gpio 發送位址 %n 的資料', 'irsend', 15, 0],
             [' ', '停止紅外線接收', 'irstop'],
