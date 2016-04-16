0cc4b0340c0fc858460b37ac7425bc56ecbb3b6b
UNU Product API <ckenson@gmail.com>
2016-04-16 14:42:14 +0800

    voiceText

--- wf8266r_extension 20160417.js
+++ wf8266r_extension 20160417.js
@@ -205,6 +205,10 @@
 
         speechSynthesis.speak(u);
     };
+    
+    ext.voiceText = function(){
+        return voiceData.Text;
+    }
 
     ext.speech_text = function () {
         if (rec == null)
@@ -216,16 +220,16 @@
         var result = "";
 
         rec.onend = function () {
-            console.log("end");
+            //console.log("end");
             rec.start();
         }
 
         rec.onstart = function () {
-            console.log("start");
+            //console.log("start");
         }
 
         rec.onerror = function (event) {
-            console.log(event);
+            //console.log(event);
         }
 
         rec.onresult = function (event) {
@@ -233,13 +237,11 @@
             if (typeof (event.results) == 'undefined') {
                 rec.onend = null;
                 rec.stop();
-                console.log("stop");
             }
 
             if (event.results.length > 0) {
                 if (event.results[event.results.length - 1].isFinal)
                     voiceData.Text = event.results[event.results.length - 1][0].transcript;
-                console.log(voiceData.Text);
             }
         }
     }
@@ -330,6 +332,7 @@
             ['w', 'HTTP %m.restfulType 從 %s', 'http', 'GET', 'http://api.thingspeak.com/apps/thinghttp/send_request?api_key=EM18B52PSHXZB4DD'],
             ['w', '說 %s', 'speak_text', 'Scratch 遇上 WF8266R'],
             [' ', '監聽語音', 'speech_text'],
+            ['r', '語音文字', 'voiceText'],
 
             [' ', '紅外線發射器，接在腳位 %d.gpio 發送位址 %n 的資料', 'irsend', 15, 0],
             [' ', '停止紅外線接收', 'irstop'],
